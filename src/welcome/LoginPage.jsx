07e721bd8d08b69208144219c156e51da44190e8
LeozeCroissant <leo.foulloy@gmail.com>
2024-12-18 12:21:05 +0100

    advancements login page - missing background shapes

--- src/welcome/LoginPage.jsx
+++ src/welcome/LoginPage.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import "./LoginPage.css";
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
 
 const LoginPage = () => {
   return (
     <div className="login-container">
+      <div className="background-pattern"></div>
       <div className="login-box">
         <div className="left-box">
           <div className="logo">
-            <img
-              src={logo}
-              alt="SubSavvy logo"
-            />
+            <img src={logo} alt="SubSavvy logo" />
           </div>
           <h1 className="login-title">Log in</h1>
           <p className="login-subtext">
-            Don't have an account?<br />
+            Don't have an account? <br />
             <span>Create your account</span>
           </p>
         </div>
+
         <div className="right-box">
           <div className="input-container">
             <input type="email" placeholder="Email" className="login-input" />
-            <input type="password" placeholder="Password" className="login-input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="login-input"
+            />
           </div>
 
           <div className="login-actions">
             <button className="google-login">
-            Or log in with
-            <img
+              Or log in with
+              <img
                 src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
                 alt="Google logo"
-                className="google-logo"
               />
             </button>
             <button className="submit-login">Log in</button>
