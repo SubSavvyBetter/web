aea00cee9ff1e3f9b6622997cd82decfb75d9271
LeozeCroissant <leo.foulloy@gmail.com>
2025-01-09 19:15:34 +0100

    Sign up info page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import LoginPage from './welcome/LoginPage'
 import SignupPageEmail from './welcome/SignupPageEmail'
+import SignupPageInfo from './welcome/SignupPageInfo'
 
 function App() {
 
-  return <SignupPageEmail />;
+  return <SignupPageInfo />;
  
 }
 
