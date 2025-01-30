2b5b1351bfb084e0ee6bbac3b5d7c95ba5f5e904
T1masv <timotei.gerante@gmail.com>
2025-01-30 16:18:32 +0100

    [Message] Update environment variable usage and adjust configurations
    Replaced hardcoded API URL with an environment variable for better flexibility and moved configuration to `vite.config.js`. Updated `.gitignore` to include `.env*` files, and made minor structural adjustments in the Sidebar component.

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -22,37 +22,37 @@ const Sidebar = () => {
                     className="w-12 h-12 m-auto cursor-pointer"
                     onClick={() => handleNavigate('/')}
                 />
-            </div>
-            <nav className="space-y-4">
-                <div
-                    className="text-gray-300 hover:text-white cursor-pointer"
-                    onClick={() => handleNavigate('/dashboard')}
-                >
-                    Dashboard
-                </div>
-                <div
-                    className="text-gray-300 hover:text-white cursor-pointer"
-                    onClick={() => handleNavigate('/notifications')}
-                >
-                    Notifications
-                </div>
-            </nav>
-            <div className="absolute bottom-8 space-y-4 m-auto">
-                <div className="w-8 h-8 rounded-full bg-gray-400 m-auto"></div>
-                <div className="flex flex-row items-center gap-3">
-                    <p className="text-gray-300">Besoin d'aide ?</p>
-                </div>
-                <div
-                    className="text-gray-300 hover:text-white cursor-pointer"
-                    onClick={() => handleNavigate('/settings')}
-                >
-                    Paramètres
-                </div>
-                <div
-                    className="text-gray-300 hover:text-white cursor-pointer"
-                    onClick={handleLogout}
-                >
-                    Se déconnecter
+                <nav className="space-y-4">
+                    <div
+                        className="text-gray-300 hover:text-white cursor-pointer"
+                        onClick={() => handleNavigate('/dashboard')}
+                    >
+                        Dashboard
+                    </div>
+                    <div
+                        className="text-gray-300 hover:text-white cursor-pointer"
+                        onClick={() => handleNavigate('/notifications')}
+                    >
+                        Notifications
+                    </div>
+                </nav>
+                <div className="absolute bottom-8 space-y-4 m-auto">
+                    <div className="w-8 h-8 rounded-full bg-gray-400 m-auto"></div>
+                    <div className="flex flex-row items-center gap-3">
+                        <p className="text-gray-300">Besoin d'aide ?</p>
+                    </div>
+                    <div
+                        className="text-gray-300 hover:text-white cursor-pointer"
+                        onClick={() => handleNavigate('/settings')}
+                    >
+                        Paramètres
+                    </div>
+                    <div
+                        className="text-gray-300 hover:text-white cursor-pointer"
+                        onClick={handleLogout}
+                    >
+                        Se déconnecter
+                    </div>
                 </div>
             </div>
         </div>
